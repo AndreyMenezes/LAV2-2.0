6822ff1d4e94cdf07a4d7c5581d163bbeb8ddd70
Alberto Fagner Ferreira de Barros <albertofagner.cav@gmail.com>
2013-02-24 14:03:38 -0300

    melhora no grafico do total de sessões por dia

--- US7-variacaoSessaoEstudoPorDia.R
+++ US7-variacaoSessaoEstudoPorDia.R
@@ -9,7 +9,9 @@ geom_segment(aes(x = "09/17/2011", y = 0, xend = "09/17/2011", yend = 150,color=
 geom_segment(aes(x = "10/29/2011", y = 0, xend = "10/29/2011", yend = 150,color="10/29/2011 - Prova 02"))+
 geom_segment(aes(x = "11/26/2011", y = 0, xend = "11/26/2011", yend = 150,color="11/26/2011 - Prova 03"))+
 geom_segment(aes(x = "12/03/2011", y = 0, xend = "12/03/2011", yend = 150,color="12/03/2011 - Prova Final"))+
-scale_colour_manual("Data das Provas", breaks = c("09/17/2011 - Prova 01", "10/29/2011 - Prova 02", "11/26/2011 - Prova 03","12/03/2011 - Prova Final"),values = c("red", "green", "blue","orange"))
+scale_colour_manual("Data das Provas", breaks = c("09/17/2011 - Prova 01", "10/29/2011 - Prova 02", "11/26/2011 - Prova 03","12/03/2011 - Prova Final"),values = c("red", "green", "blue","orange"))+
+theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank())+
+theme(axis.ticks = element_blank())
 
 
 png(filename="us7.png",bg="transparent",width = 580, height = 480)
