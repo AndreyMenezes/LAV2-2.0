6c015fe2cc674635aa04dbb51894ad540bf716ea
Alberto Fagner Ferreira de Barros <albertofagner.cav@gmail.com>
2013-02-26 00:41:42 -0300

    mudança do grafico de barras para linhas e mudança da posição da legenda

--- US7 - Analise SessãoEstudo-Dia.R
+++ US7 - Analise SessãoEstudo-Dia.R
@@ -1,17 +1,22 @@
 # Codigo gerador do grafico do total de sessões por dia durante o periodo
-# Alberto - versão 2.0 (Fevereiro 2013)
+# Alberto - versão 3.0 (Fevereiro 2013)
 
 require(ggplot2)
 sessoes<-read.csv("dados/tabelaDataSessao.csv")
+sessoes<-data.frame(sessoes,1)
 
-p<-ggplot(sessoes,aes(dataDasSessoes,sessoes))+geom_histogram()+labs(x="Datas",y="Total de Sessões")+
-geom_segment(aes(x = "09/17/2011", y = 0, xend = "09/17/2011", yend = 150,color="09/17/2011 - Prova 01"))+
-geom_segment(aes(x = "10/29/2011", y = 0, xend = "10/29/2011", yend = 150,color="10/29/2011 - Prova 02"))+
-geom_segment(aes(x = "11/26/2011", y = 0, xend = "11/26/2011", yend = 150,color="11/26/2011 - Prova 03"))+
-geom_segment(aes(x = "12/03/2011", y = 0, xend = "12/03/2011", yend = 150,color="12/03/2011 - Prova Final"))+
-scale_colour_manual("Data das Provas", breaks = c("09/17/2011 - Prova 01", "10/29/2011 - Prova 02", "11/26/2011 - Prova 03","12/03/2011 - Prova Final"),values = c("red", "green", "blue","orange"))+
+p<-ggplot(sessoes,aes(dataDasSessoes,sessoes,group=X1))+geom_line()+labs(x="Datas",y="Total de Sessões")+
+geom_segment(aes(x = "09/17/2011", y = 0, xend = "09/17/2011", yend = 150),colour="red")+
+geom_segment(aes(x = "10/29/2011", y = 0, xend = "10/29/2011", yend = 150),colour="pink")+
+geom_segment(aes(x = "11/26/2011", y = 0, xend = "11/26/2011", yend = 150),colour="blue")+
+geom_segment(aes(x = "12/03/2011", y = 0, xend = "12/03/2011", yend = 150),colour="orange")+
+theme_bw()+
 theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank())+
-theme(axis.ticks = element_blank())
+theme(axis.ticks = element_blank())+
+geom_text(data = NULL, x = 14, y = 145, label = "09/17/2011 - Prova 1",size=4,colour="red")+
+geom_text(data = NULL, x = 14, y = 140, label = "10/29/2011 - Prova 2",size=4,colour="pink")+
+geom_text(data = NULL, x = 14, y = 135, label = "11/26/2011 - Prova 3",size=4,colour="blue")+
+geom_text(data = NULL, x = 14, y = 130, label = "12/03/2011 - Prova Final",size=4,colour="orange")
 
 
 png(filename="us7.png",bg="transparent",width = 580, height = 480)
