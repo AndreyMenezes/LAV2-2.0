dc64a6b59ae4cb0ddcb89aac9770db819e5746ed
Andrey Nogueira Menezes <andrey.menezes@gmail.com>
2013-03-06 18:28:54 -0300

    Atualização US12

--- US12 - Tabela Intercorrelação Variáveis Independentes.R
+++ US12 - Tabela Intercorrelação Variáveis Independentes.R
@@ -3,8 +3,12 @@
 
 tabela1 = read.csv("dados/TabelaParaPerfis.csv")
 tabela2 = read.csv("dados/TableSessionLength.csv") # calcular numero de sessoes
-tabela3 = read.csv("dados/")
+tabela3 = read.csv("dados/exercicios-20112.csv", head=F)
 
-tabela1 = tabela1[,-3]
+tabela1 = tabela1[,c(-1,-3)]
 tabela2 <- with(tabela2,aggregate(session,list(matricula),FUN=max))
 colnames(tabela2) = c("Matricula", "NumeroSessoes")
+
+tabelaCorrelacao = cor(tabela1,use="complete.obs", method=c("spearman"))
+
+write.csv(tabelaCorrelacao, "dados/TabelaIntercorrelação.csv")
