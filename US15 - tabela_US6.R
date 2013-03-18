4ec07b52f84acdc2a5a7e57cca8c6540e35e27c4
Iury Gregory Melo Ferreira <iurygregory@gmail.com>
2013-03-18 10:40:06 -0300

    Bug US6 corrigido

--- US15 - tabela_US6.R
+++ US15 - tabela_US6.R
@@ -4,25 +4,25 @@
 # Iury Gregory Melo Ferreira - Versão 2.0 (Fevereiro 2013)
 
 
-data_ex = read.csv("dados/SubmissoesHorarioDeAula.csv",header=F)
+data_ex = read.csv("dados/SubmissoesHorarioDeAula.csv",header=T)
 data_c2 = data_ex
 for(i in seq(1:nrow(data_c2))){
   data_c2[i,"data"] = toString(data.frame(strsplit(toString(data_c2[i,"data.hora"]), " "))[1,])
 }
 
-data2 = data.frame(data_c2[,1])
+data2 = data.frame(data_c2[,"matricula"])
 data2$data = data_c2$data
-data_c2 = data2[!duplicated(data2),] #remove as linhas duplicadas, as vezes o aluno manda 10 ex. no msm dia
+data_c2 = data2[!duplicated(data2),]
 colnames(data_c2) = c("matricula","atividade")
 dias_de_aula = 125
 data_c2$atividade = 1
 data_c2 = aggregate(data_c2$atividade, list(data_c2$matricula), FUN=sum)
 colnames(data_c2) = c("matricula","atividade")
-data_c2$atividade = data_c2$atividade/dias_de_aula
+data_c2$atividade = data_c2$atividade/24
 
 write.csv(data_c2,"dados/AgrupamentoAtividadeEmAula.csv")
 
-data_fora = read.csv("dados/SubmissoesForaHorarioDeAula.csv",header=F)
+data_fora = read.csv("dados/SubmissoesForaHorarioDeAula.csv",header=T)
 data_c3 = data_fora
 for(i in seq(1:nrow(data_c3))){
   data_c3[i,"data"] = toString(data.frame(strsplit(toString(data_c3[i,"data.hora"]), " "))[1,])
@@ -30,7 +30,7 @@ for(i in seq(1:nrow(data_c3))){
 
 data3 = data.frame(data_c3[,1])
 data3$data = data_c3$data
-data_c2 = data2[!duplicated(data2),]
+#data_c3 = data3[!duplicated(data3),]
 colnames(data_c3) = c("matricula","atividade")
 data_c3$atividade = 1
 data_c3 = aggregate(data_c3$atividade, list(data_c3$matricula), FUN=sum)
