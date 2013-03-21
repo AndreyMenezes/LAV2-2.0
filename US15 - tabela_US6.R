53d8fe89728c9d5413a69578794a16b95c590afb
Andrey Nogueira Menezes <andrey.menezes@gmail.com>
2013-03-20 21:32:02 -0300

    Correção

--- US15 - tabela_US6.R
+++ US15 - tabela_US6.R
@@ -14,11 +14,11 @@ data2 = data.frame(data_c2[,"matricula"])
 data2$data = data_c2$data
 data_c2 = data2[!duplicated(data2),]
 colnames(data_c2) = c("matricula","atividade")
-dias_de_aula = 125
+dias_de_aula = 28
 data_c2$atividade = 1
 data_c2 = aggregate(data_c2$atividade, list(data_c2$matricula), FUN=sum)
 colnames(data_c2) = c("matricula","atividade")
-data_c2$atividade = data_c2$atividade/24
+data_c2$atividade = data_c2$atividade/dias_de_aula
 
 write.csv(data_c2,"dados/AgrupamentoAtividadeEmAula.csv")
 
@@ -28,13 +28,14 @@ for(i in seq(1:nrow(data_c3))){
   data_c3[i,"data"] = toString(data.frame(strsplit(toString(data_c3[i,"data.hora"]), " "))[1,])
 }
 
-data3 = data.frame(data_c3[,1])
+data3 = data.frame(data_c3[,"matricula"])
 data3$data = data_c3$data
-#data_c3 = data3[!duplicated(data3),]
+data_c3 = data3[!duplicated(data3),]
 colnames(data_c3) = c("matricula","atividade")
+dias_do_periodo = 125
 data_c3$atividade = 1
 data_c3 = aggregate(data_c3$atividade, list(data_c3$matricula), FUN=sum)
 colnames(data_c3) = c("matricula","atividade")
-data_c3$atividade = data_c3$atividade/dias_de_aula
+data_c3$atividade = data_c3$atividade/(dias_do_periodo - dias_de_aula)
 
 write.csv(data_c3,"dados/AgrupamentoAtividadeForadeAula.csv")
